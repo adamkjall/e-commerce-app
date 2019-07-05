f7decc61a93a6e2587f352984b01b8c0d03581fb
Adam <bottarn.botvidsson@gmail.com>
2019-07-05 09:25:49 +0200

    Update menu-items to navigate withRouter higher order component

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -47,8 +47,17 @@ class Directory extends React.Component {
   }
 
   render() {
-    return <div className="directory-menu">{this.state.section.map(({title, imageUrl, id, size}) =>
-      <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />)}</div>;
+    /*
+    return <div className="directory-menu">{this.state.section.map(({title, imageUrl, id, size, linkUrl}) =>
+      <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />)}</div>;
+    THIS IS EQ TO THE CODE BENEATH */
+    return (
+      <div className="directory-menu">
+        {this.state.section.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
   }
 }
 
